805f1d17a3ba83d75ec373435df4c503c9a3ef3d
biadar1 <60288396+biadar1@users.noreply.github.com>
2020-10-25 19:17:26 +0100

    Add files via upload

---
+++ test/test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace test
+{
+    class zadania
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Zadanie 2.1");
+             double celc,far;
+             celc = Convert.ToDouble(Console.ReadLine());
+             far = celc*9/5+32;
+             Console.WriteLine(far);
+
+             Console.WriteLine("Zadanie 2.2");
+             double a, b, c, delta;
+             Console.WriteLine("Podaj A");
+             a = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Podaj B");
+             b = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Podaj C");
+             c = Convert.ToDouble(Console.ReadLine());
+             delta=b*b-4*a*c;
+             Console.WriteLine("Delta wynosi " + delta);        
+
+             Console.WriteLine("Zadanie 2.3");
+             double m, w, bmi;
+             Console.WriteLine("Podaj swoją masę w kilogramach");
+             m = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Podaj swój wzrost w METRACH");
+             w = Convert.ToDouble(Console.ReadLine());
+             bmi = m / ( w * w );
+             Console.WriteLine("Twoje BMI wynosi: " + bmi);
+
+            Console.WriteLine("Zadanie 2.4 odpowiedź: a");
+            Console.WriteLine("Zadanie 2.5 odpowiedź: d");
+            Console.WriteLine("Zadanie 2.6 odpowiedź: 3");
+            Console.WriteLine("Zadanie 2.7 odpowiedź: 7");
+            Console.WriteLine("Zadanie 2.8 odpowiedź: a");
+
+        }
+    }
+}
